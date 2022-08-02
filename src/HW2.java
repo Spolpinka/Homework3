c9cf8b143a1d8dfaf2c09b19965ad679be438b9b
Spolpinka <a421243266@gmail.com>
2022-08-02 19:43:26 +0300

    homework 3, part 1

---
+++ src/HW2.java
@@ -0,0 +1,14 @@
+public class HW2 {
+    public static void main(String[] args) {
+        byte first = 1;
+        short second = 129;
+        int third = 32768;
+        long forth = 2147483648L;
+
+        float variableFloat = 2.5F;
+        double variableDouble = 2.5;
+
+        char variableChar = 0;
+        boolean variableBoolean = true;
+    }
+}
