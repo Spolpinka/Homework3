d389694e41d66f94060ed591e597c0c61d18acda
Spolpinka <a421243266@gmail.com>
2022-08-02 19:48:02 +0300

    homework 3, part 1

--- src/HW2.java
+++ src/HW2.java
@@ -1,14 +1,16 @@
 public class HW2 {
     public static void main(String[] args) {
+        //целочисленные
         byte first = 1;
         short second = 129;
         int third = 32768;
         long forth = 2147483648L;
-
+        //с плавающей запятой
         float variableFloat = 2.5F;
         double variableDouble = 2.5;
-
+        //символьная
         char variableChar = 0;
+        //логическая
         boolean variableBoolean = true;
     }
 }
