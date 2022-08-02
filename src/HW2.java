036166a99803d094154c527227a38a7cd4371264
Spolpinka <a421243266@gmail.com>
2022-08-02 21:14:28 +0300

    homework 3, part 2

--- src/HW2.java
+++ src/HW2.java
@@ -2,17 +2,11 @@ import javax.sound.midi.Soundbank;
 
 public class HW2 {
     public static void main(String[] args) {
-        byte first = 1;
-        short second = 129;
-        int third = 32768;
-        long forth = 2147483648L;
-        //с плавающей запятой
-        float variableFloat = 2.5F;
-        double variableDouble = 2.5;
-        //символьная
-        char variableChar = 0;
-        //логическая
-        boolean variableBoolean = true;
+        double firstBoxerWeight = 78.2;
+        double secondBoxerWeight = 82.7;
+        System.out.println("Общий вес двух боксеров - " + (firstBoxerWeight+secondBoxerWeight) + " кг.");
+        System.out.println("Разница в весе боксеров - " + (secondBoxerWeight-firstBoxerWeight) + " кг.");
+
 
     }
 }
