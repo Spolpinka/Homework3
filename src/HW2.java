52151e27fae9025e897e7ff77e9c0fe216680beb
Spolpinka <a421243266@gmail.com>
2022-08-02 21:07:10 +0300

    homework 3, part 1

--- src/HW2.java
+++ src/HW2.java
@@ -2,6 +2,17 @@ import javax.sound.midi.Soundbank;
 
 public class HW2 {
     public static void main(String[] args) {
+        byte first = 1;
+        short second = 129;
+        int third = 32768;
+        long forth = 2147483648L;
+        //с плавающей запятой
+        float variableFloat = 2.5F;
+        double variableDouble = 2.5;
+        //символьная
+        char variableChar = 0;
+        //логическая
+        boolean variableBoolean = true;
 
     }
 }
